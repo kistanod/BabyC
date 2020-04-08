856a47a7d8138b5902df127a926636d590f9bb93
kistanod <kistanod@kistanods-MacBook-Pro.local>
2020-04-07 22:39:08 -0700

    fixed regex

--- driver.c
+++ driver.c
@@ -8,8 +8,8 @@ extern int yyparse();
 // A global variable that holds a pointer to the AST root
 ASTNode* gASTRoot;
 
-int main(int argc, char**argv)
-{
+int main(int argc, char**argv) {
+
         if (argc != 2){
                  printf("ERROR: Invalid number of command-line arguments. Usage: bcc File_Name.bc\n");
                  exit(1);
@@ -23,11 +23,9 @@ int main(int argc, char**argv)
         // Call the parser. 
         // Add embedded actions to the parser (in BabyC.y) to construct the AST and store its root in gASTRoot. 
         yyparse();
-        fclose(yyin);
 
-        return 1;
+        
 
-        // Now that the AST has been constructed, pass its root to the function that traverses it and generates the ILOC code.
-        // GenerateILOC(gASTRoot);
-        // Code generation is commented out in this assignment. You will implement it in the next assignment. 		
+
+        fclose(yyin);		
 }
